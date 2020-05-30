baa7ce03090db0ed53fe7a042ab65535e966a269
Javier Martinez <javi@flamingalah.net>
2020-05-30 21:16:09 +0200

    Implementation of makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  
+  setInverse <- function() {
+    inverse <<- solve(x)
+  }
+  
+  getInverse <- function() inverse
+  
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
